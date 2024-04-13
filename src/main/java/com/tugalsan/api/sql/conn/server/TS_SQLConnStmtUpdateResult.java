c5fae80d67f80847fcd7ec28f9d2fcab886eae02
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-13 17:46:40 +0300

    Update TS_SQLConnStmtUpdateResult.java

--- src/main/java/com/tugalsan/api/sql/conn/server/TS_SQLConnStmtUpdateResult.java
+++ src/main/java/com/tugalsan/api/sql/conn/server/TS_SQLConnStmtUpdateResult.java
@@ -2,4 +2,7 @@ package com.tugalsan.api.sql.conn.server;
 
 public record TS_SQLConnStmtUpdateResult(int affectedRowCount, Long autoId) {
 
+    public static TS_SQLConnStmtUpdateResult of(int affectedRowCount, Long autoId) {
+        return new TS_SQLConnStmtUpdateResult(affectedRowCount, autoId);
+    }
 }
