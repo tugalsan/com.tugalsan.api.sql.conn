fccfa09dedf56fd919e87e452c93ffa6e1dd013a
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-01-10 06:13:41 +0300

    .

--- src/main/java/com/tugalsan/api/sql/conn/server/TS_SQLConnAnchorUtils.java
+++ src/main/java/com/tugalsan/api/sql/conn/server/TS_SQLConnAnchorUtils.java
@@ -4,6 +4,7 @@ import com.tugalsan.api.file.json.server.*;
 import com.tugalsan.api.file.server.*;
 import com.tugalsan.api.file.txt.server.*;
 import com.tugalsan.api.log.server.*;
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
 import com.tugalsan.api.unsafe.client.*;
 import java.nio.file.*;
 
@@ -11,7 +12,7 @@ public class TS_SQLConnAnchorUtils {
 
     final private static TS_Log d = TS_Log.of(TS_SQLConnAnchorUtils.class);
 
-    public static TS_SQLConnAnchor createAnchor(Path dir, CharSequence dbName) {
+    public static TGS_UnionExcuse<TS_SQLConnAnchor> createAnchor(Path dir, CharSequence dbName) {
         TS_DirectoryUtils.assureExists(dir);
         var filePath = dir.resolve(TS_SQLConnConfig.class.getSimpleName() + "_" + dbName + ".json");
         d.cr("createAnchor", filePath);
@@ -33,7 +34,10 @@ public class TS_SQLConnAnchorUtils {
         });
         d.ci("createAnchor", jsonString);
 
-        var config = TS_FileJsonUtils.toObject(jsonString, TS_SQLConnConfig.class);
-        return TS_SQLConnAnchor.of(config);
+        var u_config = TS_FileJsonUtils.toObject(jsonString, TS_SQLConnConfig.class);
+        if (u_config.isExcuse()) {
+            return TGS_UnionExcuse.ofExcuse(u_config.excuse());
+        }
+        return TGS_UnionExcuse.of(TS_SQLConnAnchor.of(u_config.value()));
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,9 @@ module com.tugalsan.api.sql.conn {
     requires java.sql;
     requires tomcat.jdbc;
     requires com.tugalsan.api.unsafe;
-    
+    requires com.tugalsan.api.union;
     requires com.tugalsan.api.profile;
     requires com.tugalsan.api.function;
-    
     requires com.tugalsan.api.string;
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.list;
@@ -14,7 +13,6 @@ module com.tugalsan.api.sql.conn {
     requires com.tugalsan.api.file.obj;
     requires com.tugalsan.api.file.json;
     requires com.tugalsan.api.file.txt;
-    requires com.tugalsan.api.union;
     requires com.tugalsan.api.tuple;
     requires com.tugalsan.api.sql.col.typed;
     requires com.tugalsan.api.sql.sanitize;
