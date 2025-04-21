c64397e57edd2ff597ed8d40c212f8b580083bb0
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-04-21 09:51:50 +0300

    Update TS_SQLConnPoolUtils.java

--- src/main/java/com/tugalsan/api/sql/conn/server/TS_SQLConnPoolUtils.java
+++ src/main/java/com/tugalsan/api/sql/conn/server/TS_SQLConnPoolUtils.java
@@ -3,14 +3,13 @@ package com.tugalsan.api.sql.conn.server;
 import org.apache.tomcat.jdbc.pool.PoolProperties;
 
 public class TS_SQLConnPoolUtils {
-    
-    private TS_SQLConnPoolUtils(){
-        
+
+    private TS_SQLConnPoolUtils() {
+
     }
 
     public static PoolProperties create(TS_SQLConnConfig config) {
         var pool = new PoolProperties();
-
         pool.setUrl(TS_SQLConnURLUtils.create(config));
         pool.setDriverClassName(TS_SQLConnMethodUtils.getDriver(config));
         if (config.dbUser == null || config.dbUser.equals("") || config.dbPassword == null) {
@@ -23,25 +22,26 @@ public class TS_SQLConnPoolUtils {
         pool.setMaxIdle(maxActive / 10);
         pool.setInitialSize(maxActive / 10);
         pool.setValidationInterval(30000);
-        pool.setTimeBetweenEvictionRunsMillis(60000);
+        pool.setTimeBetweenEvictionRunsMillis(30000);
         pool.setMaxWait(10000);
         pool.setMinEvictableIdleTimeMillis(30000);
         pool.setMinIdle(10);
-        pool.setRemoveAbandoned(true);
-        pool.setRemoveAbandonedTimeout(600);
-        pool.setFairQueue(true);
         pool.setLogAbandoned(true);
+        pool.setRemoveAbandoned(true);
+        pool.setRemoveAbandonedTimeout(60);
+        pool.setFairQueue(true);
         pool.setJmxEnabled(true);
         pool.setTestOnBorrow(true);
-        pool.setTestOnConnect(true);
+        //pool.setTestOnConnect(true);
         pool.setTestWhileIdle(true);
         pool.setTestOnReturn(false);
-        pool.setTestWhileIdle(true);
+        //pool.setTestWhileIdle(false);
         pool.setValidationQuery("SELECT 1");
         pool.setJdbcInterceptors(
                 "org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;"
-                + "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer;"
-                + "org.apache.tomcat.jdbc.pool.interceptor.ResetAbandonedTimer");
+                //+ "org.apache.tomcat.jdbc.pool.interceptor.ResetAbandonedTimer;"
+                + "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer");
+
         return pool;
     }
 }
